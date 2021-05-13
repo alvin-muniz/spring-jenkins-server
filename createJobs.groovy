e3deafa4639a593241f9c9c2765fa416edc09f24
Alvin Muniz <muniz.alvin@gmail.com>
2021-05-13 12:43:38 -0500

    createJobs groovy file added

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,5 +1,15 @@
-pipelineJob('pipelineJob') {
+pipelineJob('spring-jenkins-demo') {
     definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/alvin-muniz/spring-jenkins-demo.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
         cps {
             script(readFileFromWorkspace('pipelineJob.groovy'))
             sandbox()
